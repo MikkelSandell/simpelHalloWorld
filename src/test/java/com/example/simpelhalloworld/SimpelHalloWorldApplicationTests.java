aa5a1ed997a8246049870930a54b6f71f7275159
Bruger <mikk00j3@stud.kea.dk>
2022-08-26 08:57:49 +0200

    first commit

---
+++ src/test/java/com/example/simpelhalloworld/SimpelHalloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpelhalloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpelHalloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
