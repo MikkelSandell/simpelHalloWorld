b64672aeb5415228d2423d39d2721cb91c0dfd40
MikkelSandell <90551694+MikkelSandell@users.noreply.github.com>
2023-01-09 15:46:32 +0100

    Update SimpelHalloWorldApplicationTests.java

--- src/test/java/com/example/simpelhalloworld/SimpelHalloWorldApplicationTests.java
+++ src/test/java/com/example/simpelhalloworld/SimpelHalloWorldApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.simpelhalloworld;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SimpelHalloWorldApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
